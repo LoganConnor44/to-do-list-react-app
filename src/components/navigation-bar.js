9590420d2f7141e6cea71badf17c2b649b4cb2bf
Logan Connor <logan.connor@floridadep.gov>
2019-12-17 13:13:39 -0500

    updating styling for the appbar / navigation bar

--- src/components/navigation-bar.js
+++ src/components/navigation-bar.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { fade, makeStyles } from '@material-ui/core/styles';
+import React, {useState} from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
@@ -7,101 +6,54 @@ import Typography from '@material-ui/core/Typography';
 import InputBase from '@material-ui/core/InputBase';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
+import '../styles/navigation.css';
 
-const useStyles = makeStyles(theme => ({
-  grow: {
-    flexGrow: 1
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    display: 'none',
-    [theme.breakpoints.up('sm')]: {
-      display: 'block',
-    },
-  },
-  search: {
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
-    },
-    marginRight: theme.spacing(2),
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(3),
-      width: 'auto',
-    },
-  },
-  searchIcon: {
-    width: theme.spacing(7),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  inputRoot: {
-    color: 'inherit',
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 7),
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: 200,
-    },
-  },
-  sectionDesktop: {
-    display: 'none',
-    [theme.breakpoints.up('md')]: {
-      display: 'flex',
-    },
-  },
-  sectionMobile: {
-    display: 'flex',
-    [theme.breakpoints.up('md')]: {
-      display: 'none',
-    },
-  },
-}));
+const NavigationBar = () => {
+	const [isSearchIconClicked, setIsSearchIconClicked] = useState(false);
 
-export default function NavigationBar() {
-  const classes = useStyles();
-  return (
-    <div className={classes.grow}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="open drawer"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography className={classes.title} variant="h6" noWrap>
-            PWA React To Do App
-          </Typography>
-          <div className={classes.search}>
-            <div className={classes.searchIcon}>
-              <SearchIcon />
-            </div>
-            <InputBase
-              placeholder="Search…"
-              classes={{
-                root: classes.inputRoot,
-                input: classes.inputInput,
-              }}
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </div>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+	const searchIconClick = () => {
+		setIsSearchIconClicked(!isSearchIconClicked);
+	}
+
+	return (
+		<AppBar>
+			<Toolbar>
+				<container-left>
+					<hamburger-styling>
+						<IconButton
+							color="inherit"
+							aria-label="open drawer" >
+							<MenuIcon />
+						</IconButton>
+					</hamburger-styling>
+					<title-styling>
+						<Typography variant="h6" noWrap>
+							PWA To Do App
+						</Typography>
+					</title-styling>
+				</container-left>
+				<container-right>
+				{
+					isSearchIconClicked === false ? (
+						<search-icon-styling>
+							{/* below classes taken from raw, inspecting html - could not get icons to match*/}
+							<div class="MuiButtonBase-root MuiIconButton-root MuiIconButton-colorInherit">
+								<SearchIcon onClick={searchIconClick} />
+							</div>
+						</search-icon-styling>
+					) : (
+						<search-input-styling>
+							<InputBase
+								placeholder="Search…"
+								inputProps={{ 'aria-label': 'search' }}
+								className="rootInput" />
+						</search-input-styling>
+					)
+				}
+				</container-right>
+			</Toolbar>
+		</AppBar>
+	);
 }
+
+export default NavigationBar;
