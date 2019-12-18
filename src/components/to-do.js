5cef3025b87be306aec5bbbf7e942172e2a1e9e4
Logan Connor <logan.connor@floridadep.gov>
2019-12-18 11:55:50 -0500

    added more documentation

--- src/components/to-do.js
+++ src/components/to-do.js
@@ -88,7 +88,7 @@ const Todo = () => {
      */
     const addTask = name => {
         const newUserTask = { 
-            name,
+            name: name,
             status: StatusEnum.ACTIVE,
             created: Date.now(),
             deadline: null,
@@ -109,26 +109,46 @@ const Todo = () => {
     /**
      * Sets an existing task to completed.
      * 
-     * @param {Integer} index 
+     * @param { {id: integer, index: integer} } selectedTask
      */
-    const completeTask = (index, task) => {
-        const existingTasks = [...tasks];
-        existingTasks[index].status = StatusEnum.INACTIVE;
-        setTasks(existingTasks);
-        db.goals.update(task.id, {status: StatusEnum.INACTIVE});
+    const completeTask = selectedTask => {
+        const existingTasks = toggleTaskStatusIndicator([selectedTask]);
+        db.goals.update(selectedTask.id, {status: existingTasks[selectedTask.index].status});
     };
+
+    /**
+     * Toggles a task's status enum.
+     * 
+     * Retrieves the existing tasks.
+     * Iterates through the passed in selected tasks and sets the appropriate status.
+     * Sets the tasks to the updated ones.
+     * Returns the updated tasks.
+     * @param { [ {id: integer, index: integer} ] } selectedTasks 
+     * @returns { [ {id: integer, index: integer} ] }
+     */
+    const toggleTaskStatusIndicator = selectedTasks => {
+        const existingTasks = [...tasks];
+        selectedTasks.forEach(task => {
+            if (existingTasks[task.index].status === StatusEnum.INACTIVE) {
+                existingTasks[task.index].status = StatusEnum.ACTIVE;
+            } else if (existingTasks[task.index].status === StatusEnum.ACTIVE) {
+                existingTasks[task.index].status = StatusEnum.INACTIVE;
+            }
+        });
+        setTasks(existingTasks);
+        return existingTasks;
+    }
 
     /**
      * Removes an existing task from the application state.
      * 
-     * @param {Integer} index 
+     * @param { {id: integer, index: integer} } selectedTask 
      */
-    const removeTask = (index, task) => {
-        console.log(`the index ${index} and the id ${task.id}`);
+    const removeTask = selectedTask => {
         const existingTasks = [...tasks];
-        existingTasks.splice(index, 1);
+        existingTasks.splice(selectedTask.index, 1);
         setTasks(existingTasks);
-        db.goals.where("id").equals(task.id).delete()
+        db.goals.where("id").equals(selectedTask.id).delete()
     };
 
     const batchRemoveTasks = selectedTasks => {
@@ -137,22 +157,25 @@ const Todo = () => {
             db.goals.bulkDelete(keysToDelete);
             db.goals.toArray().then(x => setTasks(x));
         }).then(() => {
-            console.log('job well done there friend');
+            console.info(`The selected tasks, ${JSON.stringify(selectedTasks)}, were saved to the browser db.`);
         }).catch(err => {
+            console.debug(`There was an error when removing the selected tasks, ${JSON.stringify(selectedTasks)}, from the browser db.`);
             console.error(err.stack);
         });
     };
 
     const batchCompleteTasks = selectedTasks => {
-        const keysToComplete = selectedTasks.map(x => x.id);
+        const existingTasks = toggleTaskStatusIndicator(selectedTasks);        
+        
         db.transaction('rw', db.goals, async () => {
-            for (let i = 0; i < selectedTasks.length; i++) {
-                db.goals.update(selectedTasks[i].id, { status: StatusEnum.INACTIVE });
-            }
+            selectedTasks.forEach(task => {
+                db.goals.update(task.id, {status: existingTasks[task.index].status});
+            });
             db.goals.toArray().then(x => setTasks(x));
         }).then(() => {
-            console.log('job well done there friend');
+            console.info(`The selected tasks, ${JSON.stringify(selectedTasks)}, were set as complete in the browser db.`);
         }).catch(err => {
+            console.debug(`There was an error when updating the selected tasks, ${JSON.stringify(selectedTasks)}, to the browser db.`)
             console.error(err.stack);
         });
     };
@@ -162,12 +185,11 @@ const Todo = () => {
         const syncData = async () => {
             setIsLoading(true);
             for (let i = 0; i < tasksToSync.length; i++) {
-                const response = await axios.post("http://localhost:8080/to-do/goal", tasksToSync[i]).then(function (response) {
+                const response = await axios.post("http://localhost:8080/to-do/goal", tasksToSync[i]).then(response => {
                     console.log(response);
-                    })
-                    .catch(function (error) {
+                }).catch(function (error) {
                     console.log(error);
-                    });
+                });
                 //setTasks([response.data]);
             }
             setIsLoading(false);
@@ -217,6 +239,7 @@ const Todo = () => {
                     </Grid>
                 </Grid>
             </div>
+
         </div>
     );
 };
