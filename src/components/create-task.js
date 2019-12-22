893f5ea640508658ef7be402614768a89f7b340a
Logan Connor <loganconnor44@gmail>
2019-12-22 15:52:04 -0500

    fixing the input text colour

--- src/components/create-task.js
+++ src/components/create-task.js
@@ -1,49 +1,49 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
-
-const useStyles = makeStyles(theme => ({
-    root: {
-      '& > *': {
-        margin: theme.spacing(1),
-        width: 200,
-        background: "white"
-      },
-    },
-  }));
+import '../styles/create-task.css';
+import SendIcon from '@material-ui/icons/Send';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
 
 /**
  * React create task web component.
  * 
  * @param {function} addTask 
  */
-const CreateTask = ({addTask}) => {
-    const classes = useStyles();
-    const [value, setValue] = useState("");
+const CreateTask = ({ addTask }) => {
+	const [value, setValue] = useState("");
 
-    const handleSubmit = event => {
-        event.preventDefault();
-        if (!value) {
-            return;
-        }
-        addTask(value);
-        setValue("");
-    };
+	const handleSubmit = event => {
+		event.preventDefault();
+		if (!value) {
+			return;
+		}
+		addTask(value);
+		setValue("");
+	};
 
-    return(
-        <form onSubmit = {handleSubmit}
-            className={classes.root}
-            noValidate
-            autoComplete="off">
-            <TextField 
-                label = "Task"
-                type = "text"
-                className = "input"
-                value = {value}
-                placeholder = "Add a new task"
-                onChange = {event => setValue(event.target.value)} />
-        </form>
-    );
+	return (
+		<form-styling>
+			<form onSubmit={handleSubmit}
+				noValidate
+				autoComplete="off">
+				<TextField
+					label="Task"
+					type="text"
+					className="input asdf"
+					value={value}
+					placeholder="Add a new task"
+					onChange={event => setValue(event.target.value)}
+					style={{ color: "rgba(0, 0, 0, 0.87)"}} />
+				<Tooltip title="save" >
+					<IconButton aria-label="save" type="input">
+						<SendIcon />
+					</IconButton>
+				</Tooltip>
+			</form>
+		</form-styling>
+	);
 };
 
 export default CreateTask;
