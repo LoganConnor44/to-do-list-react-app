0691a44da1f927b85e308bf585fc09cfe5b880ea
Logan Connor <loganconnor44@gmail>
2020-01-04 13:06:52 -0500

    adding installation functionality

--- src/components/navigation/app-drawer.js
+++ src/components/navigation/app-drawer.js
@@ -8,6 +8,21 @@ import Divider from '@material-ui/core/Divider';
 import FaceIcon from '@material-ui/icons/Face';
 
 const AppDrawer = ({isOpen, toggleDrawer}) => {
+
+	const [prompt, promptToInstall] = AddToHomescreenPrompt();
+
+	/**
+     * Determines if the prompt is false or anything else.
+     * Prompt may be an object such as { isTrusted: boolean }
+     */
+	const promptEventIsReady = () => prompt !== false ? prompt : false;
+
+	const installViaSideBar = () => {
+		if (promptEventIsReady()) {
+			promptToInstall();
+		}
+	};
+
 	return(
 		<Drawer open={isOpen}
 			onClose={toggleDrawer} >
@@ -22,12 +37,12 @@ const AppDrawer = ({isOpen, toggleDrawer}) => {
 				<Divider />
 				<List>
 					<ListItem>
-						<ListItemIcon>
+						<ListItemIcon onClick={installViaSideBar}>
 							<GetAppIcon />
 						</ListItemIcon>
 					</ListItem>
 				</List>
-			</div>	
+			</div>
 		</Drawer>
 	);
 };
